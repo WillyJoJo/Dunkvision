d621712e3270173c16eb61c43d7492e83ff96389
WillyJojo8 <lacasitos1333@gmail.com>
2025-02-25 02:34:17 +0100

    Metodo para jugador_partido y para datos enfrentamiento

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,3 +1,4 @@
+import requests
 from app import app
 from app import db  #Importante importar la base de datos
 from flask import jsonify
@@ -14,3 +15,54 @@ def hello_world():
 def importar_equipos():
     importar_equipos_nba()
     return jsonify({'message': 'Equipos importados exitosamente!'}), 200
+
+@app.route('/api/nba/stats_partido', methods=['GET'])
+def obtener_stats_partido_nba():
+    game_id = "0022400820"  # TIMBERWOLVES VS. THUNDER 2025-02-23
+    url = "https://stats.nba.com/stats/boxscoretraditionalv2"
+    params = {
+        "GameID": game_id,
+        "StartPeriod": 1,
+        "EndPeriod": 10,
+        "StartRange": 0,
+        "EndRange": 55800,
+        "RangeType": 2
+    }
+    
+    headers = {
+        "Host": "stats.nba.com",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Accept": "application/json, text/plain, */*",
+        "Referer": "https://www.nba.com/",
+        "Connection": "keep-alive"
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    
+    if response.status_code == 200:
+        return jsonify(response.json()), 200
+    else:
+        return jsonify({'error': 'No se pudieron obtener los datos de la NBA'}), response.status_code
+    
+@app.route('/api/nba/stats_resumen_partido', methods=['GET'])
+def obtener_resumen_partido_nba():
+    game_id = "0022400820"  # TIMBERWOLVES VS. THUNDER 2025-02-23
+    url = "https://stats.nba.com/stats/boxscoresummaryv2"
+    params = {
+        "GameID": game_id
+    }
+    
+    headers = {
+        "Host": "stats.nba.com",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Accept": "application/json, text/plain, */*",
+        "Referer": "https://www.nba.com/",
+        "Connection": "keep-alive"
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    
+    if response.status_code == 200:
+        return jsonify(response.json()), 200
+    else:
+        return jsonify({'error': 'No se pudieron obtener los datos de la NBA'}), response.status_code
